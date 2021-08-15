5fa15c57d46f2b5ddd45287b4708a241808d67f5
Romina Pineda <romi.macka@gmail.com>
2021-08-15 09:21:13 -0400

    final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2> Agrega una tarea a la lista</h2>
+    <input type="text" name="addTodo" id="addTodo" v-model="addToList" placeholder="ej. estudiar vue js">
+    <button type="submit" @click="addItem">Agregar</button>
+    <h1> Lista de tareas </h1>
+    <ul id="todoList">
+      <li v-for="(item, index) in list" :key="item">{{index+1}} : {{item}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+  name: "ToDo",
+  el: '#todoList',
+  data() {
+        return {
+          addToList: '',
+          list: [
+            'Regar las plantas',
+            'Limpiar areneros',
+          ]
+        }
+  }, 
+  methods:{
+    addItem(){
+      this.list.push(this.addToList)
+      this.addToList = ''
+    }
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  ul{
+    list-style: none;
+    text-align: start;
+  }
 </style>
